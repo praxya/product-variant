880e0fef316d5e72e2d4600f7caa803e8ef0c28f
Rubén Cabrera <rcabrera@praxya.com>
2017-12-11 11:58:19 +0100

    WIP Start module: supplier info for customer per variant

---
+++ product_variant_supplierinfo_for_customer/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# © 2015 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Product customer info per variant',
+    'summary': 'Customer info to product variant scope',
+    'version': '8.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Tecnativa, Akretion, Rubén Cabrera (Praxya),'
+              'Odoo Community Association (OCA)',
+    'category': 'Product Management',
+    'depends': [
+        'purchase',
+        'product_supplierinfo_for_customer',
+    ],
+    'data': [
+        'views/product_product_view.xml',
+        'views/product_supplierinfo_view.xml',
+    ],
+    'demo': [
+        'demo/product_demo.xml',
+        'demo/procurement_demo.xml',
+    ],
+    'installable': True,
+}
